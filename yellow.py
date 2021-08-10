c8ff517bab6a5ad15f4420e594fe71cc7b50c8d3
Chandler Swift <chandler@chandlerswift.com>
2021-08-09 22:26:31 -0500

    Add first attempt at remainder of yellow cards
    (many still need work!)

--- yellow.py
+++ yellow.py
@@ -3,7 +3,7 @@ import wikipedia
 
 def collect_links_from_wikipedia_page(s):
     for link in wikipedia.page(s).links:
-        if ' ' not in link and link.upper() != link:
+        if link.upper() != link and all(chr.isalpha() or chr == " " for chr in link):
             yield link
 
 
@@ -94,3 +94,121 @@ def collect_links_from_wikipedia_page(s):
 def y22():
     """A type of plant"""
     return collect_links_from_wikipedia_page("List of plants by common name")
+
+def y23():
+    """A type of animal"""
+    return collect_links_from_wikipedia_page("List of animal names")
+
+def y24():
+    """The name of a country"""
+    # from https://en.wikipedia.org/wiki/List_of_sovereign_states
+    return ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola',
+    'Antigua and Barbuda', 'Argentina', 'Armenia', 'Australia', 'Austria',
+    'Azerbaijan', 'The Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus',
+    'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina',
+    'Botswana', 'Brazil', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi',
+    'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Central African Republic',
+    'Chad', 'Chile', 'China', 'Colombia', 'Comoros',
+    'Democratic Republic of the Congo', 'Republic of the Congo', 'Costa Rica',
+    'Croatia', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti',
+    'Dominica', 'Dominican Republic', 'East Timor', 'Ecuador', 'Egypt',
+    'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Eswatini',
+    'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'The Gambia', 'Georgia',
+    'Germany', 'Ghana', 'Greece', 'Grenada', 'Guatemala', 'Guinea',
+    'Guinea-Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hungary', 'Iceland',
+    'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Israel', 'Italy',
+    'Ivory Coast', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya',
+    'Kiribati', 'North Korea', 'South Korea', 'Kuwait', 'Kyrgyzstan', 'Laos',
+    'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein',
+    'Lithuania', 'Luxembourg', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives',
+    'Mali', 'Malta', 'Marshall Islands', 'Mauritania', 'Mauritius', 'Mexico',
+    'Micronesia', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Morocco',
+    'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands',
+    'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'North Macedonia', 'Norway',
+    'Oman', 'Pakistan', 'Palau', 'Palestine', 'Panama', 'Papua New Guinea',
+    'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Qatar', 'Romania',
+    'Russia', 'Rwanda', 'Saint Kitts and Nevis', 'Saint Lucia',
+    'Saint Vincent and the Grenadines', 'Samoa', 'San Marino',
+    'São Tomé and Príncipe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles',
+    'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'Solomon Islands',
+    'Somalia', 'South Africa', 'South Sudan', 'Spain', 'Sri Lanka', 'Sudan',
+    'Suriname', 'Sweden', 'Switzerland', 'Syria', 'Tajikistan', 'Tanzania',
+    'Thailand', 'Togo', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey',
+    'Turkmenistan', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates',
+    'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Vanuatu',
+    'Vatican City', 'Venezuela', 'Vietnam', 'Yemen', 'Zambia', 'Zimbabwe']
+
+def y25():
+    """The name of a game"""
+    return collect_links_from_wikipedia_page('List of abstract strategy games') \
+         + collect_links_from_wikipedia_page('List of board games')
+
+def y26():
+    """The title of a movie"""
+    # TODO: this will return many false positives. Either scan manually or get
+    # a better data source?
+    return collect_links_from_wikipedia_page('List of films considered the best')
+
+def y27():
+    """The title of a TV show"""
+    links = []
+    for startswith in 'numbers A B C D E F G H I-J K-L M N O P Q-R S T U-V-W X-Y-Z'.split():
+        links += collect_links_from_wikipedia_page(f"List of television programs: {startswith}")
+    return links
+
+def y28():
+    """The name of a brand"""
+    links = []
+    for list in collect_links_from_wikipedia_page('Lists of brands'):
+        links += collect_links_from_wikipedia_page(list)
+    return links
+
+def y29():
+    """A type of profession"""
+    categories = ['healthcare',
+ 'mental health',
+ 'artistic',
+ 'dance',
+ 'entertainer',
+ 'film and television',
+ 'writing',
+ 'industrial',
+ 'metalworking',
+ 'railway industry',
+ 'scientific']
+    links = []
+    for category in categories:
+        links += collect_links_from_wikipedia_page(f"List of {category} occupations")
+    return links
+
+def y30():
+    """A type of food"""
+    links = []
+    for list in [link for link in collect_links_from_wikipedia_page('Lists of foods') if link.startswith('List of ')]:
+        links += collect_links_from_wikipedia_page(list)
+    return links
+
+def y31():
+    """An article of clothing"""
+    links = []
+    for link in collect_links_from_wikipedia_page("Index of fashion articles"):
+        link = link.split(' (')[0] # Belt (clothing) => Belt
+        if any(chr.isnumeric() for chr in link):
+            pass
+        links.append(link)
+    return links
+
+def y32():
+    """An item found in a kitchen"""
+    return collect_links_from_wikipedia_page('List of food preparation utensils')
+
+def y33():
+    """An item found at the office"""
+    return collect_links_from_wikipedia_page('Office Supplies')
+
+def y34():
+    """An object visible from here"""
+    # This one won't be generally solvable. But here are some things we can
+    # generally assume!
+    return ['card', 'cards', 'deck', 'box', 'player', 'friend', 'floor',
+    'person', 'hand', 'foot', 'arm', 'leg', 'face']
